c7c8c4a903f7852d5ca5e93145e8394274991019
Jules <blob.jules@gmail.com>
2021-02-08 16:26:14 +0100

    feat(shop): scrape new products

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,5 +1,9 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const MudJeansbrand = require('./sources/MudJeansbrand');
+const AdresseParisbrand = require('./sources/AdresseParisbrand');
+
+const pages = await dedicatedbrand.getPages('https://www.dedicatedbrand.com');
 
 async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   try {
@@ -16,6 +20,41 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   }
 }
 
-const [,, eshop] = process.argv;
 
-sandbox(eshop);
+async function steelbox (ishop = 'https://adresse.paris/602-nouveautes') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${ishop} source`);
+
+    const products = await AdresseParisbrand.scrape(ishop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+async function silverbox (yshop = 'https://mudjeans.eu/collections/men-buy-jeans') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${yshop} source`);
+
+    const products = await MudJeansbrand.scrape(yshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+const [,, eshop] = process.argv;
+const [,, ishop] = process.argv;
+const [,, yshop] = process.argv;
+
+//sandbox(eshop);
+//steelbox(ishop);
+silverbox(yshop);
