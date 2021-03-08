32efe68c86d354b617e3e09678d0f54b291b8ad7
Jules <blob.jules@gmail.com>
2021-03-08 15:53:16 +0100

    feat(new-products): insert all products

--- server/sources/dedicatedbrand.js
+++ server/sources/dedicatedbrand.js
@@ -1,75 +1,49 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-
-const DEDICATED_BRAND = ('https://www.dedicatedbrand.com');
+const {'v5': uuidv5} = require('uuid');
 
 /**
- * Parse webpage e-shop
+ * Parse webpage restaurant
  * @param  {String} data - html response
- * @return {Array} products
+ * @return {Object} restaurant
  */
 const parse = data => {
   const $ = cheerio.load(data);
 
   return $('.productList-container .productList')
     .map((i, element) => {
-      const name = $(element)
-        .find('.productList-title')
-        .text()
-        .trim()
-        .replace(/\s/g, ' ');
-      const price = parseInt(
-        $(element)
-          .find('.productList-price')
+      const link = `https://www.dedicatedbrand.com${$(element)
+        .find('.productList-link')
+        .attr('href')}`;
+
+      return {
+        link,
+        'brand': 'dedicated',
+        'price': parseInt(
+          $(element)
+            .find('.productList-price')
+            .text()
+        ),
+        'name': $(element)
+          .find('.productList-title')
           .text()
-      );
-
-      return {name, price};
+          .trim()
+          .replace(/\s/g, ' '),
+        'photo': $(element)
+          .find('.productList-image img')
+          .attr('src'),
+        '_id': uuidv5(link, uuidv5.URL)
+      };
     })
     .get();
 };
 
-function parseHomePage (data) {
-  const $ = cheerio.load(data);
-
-  return $('.productList-container .productList')
-    .map((i, element) => {
-      const href = $(element).find('a').attr('href');
-
-      return `${DEDICATED_BRAND}${href}`;
-    })
-    .get();
-};
-
-
-module.exports.getPages = async function(url = DEDICATED_BRAND) {
-  const response = await axios(url);
-  const {data, status} = response;
-
-  if (status >= 200 && status < 300) {
-    return parseHomePage(data);
-  }
-
-  console.error(status);
-
-  return [];
-};
-
-
-/**
- * Scrape all the products for a given url page
- * @param  {[type]}  url
- * @return {Array|null}
- */
 module.exports.scrape = async url => {
   const response = await axios(url);
   const {data, status} = response;
-
   if (status >= 200 && status < 300) {
     return parse(data);
   }
-
   console.error(status);
-
   return null;
 };
